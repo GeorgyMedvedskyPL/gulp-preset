acbd159e6e06fe5351dff54cc007f9ec9365fa1e
Georgy Medvedsky <georgy.medvedsky@performance-lab.top>
2024-12-13 14:31:33 +0300

    feat: update config / add sitemap / add js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const gulp = require('gulp');
 const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
@@ -10,6 +12,10 @@ const cssnano = require('cssnano');
 const htmlMinify = require('html-minifier');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concatJs = require('gulp-concat');
+const sitemap = require('gulp-sitemap');
 
 const paths = {
   html: {
@@ -40,6 +46,10 @@ const paths = {
   other: {
     src: './src/{robots.txt,.htaccess,sitemap.xml}',
     dest: 'dist/',
+  },
+  js: {
+    src: './src/js/**/*.js',
+    dest: 'dist/js',
   },
 };
 
@@ -109,6 +119,39 @@ function scss() {
   }
 }
 
+function js() {
+  if (checkJsDirectory()) {
+    if (!isDev) {
+      return gulp
+        .src(paths.js.src)
+        .pipe(
+          babel({
+            presets: ['@babel/preset-env'],
+          })
+        )
+        .pipe(uglify())
+        .pipe(concatJs('bundle.js'))
+        .pipe(gulp.dest(paths.js.dest));
+    } else {
+      return gulp
+        .src(paths.js.src)
+        .pipe(
+          babel({
+            presets: ['@babel/preset-env'],
+          })
+        )
+        .pipe(concatJs('bundle.js'))
+        .pipe(gulp.dest(paths.js.dest))
+        .pipe(browserSync.reload({ stream: true }));
+    }
+  } else {
+    console.warn(
+      'Директория src/js не найдена. Сборка будет выполнена без JavaScript.'
+    );
+    return Promise.resolve();
+  }
+}
+
 function fonts() {
   return gulp
     .src(paths.fonts.src, { encoding: false })
@@ -119,6 +162,21 @@ function images() {
   return gulp
     .src(paths.images.src, { encoding: false })
     .pipe(gulp.dest(paths.images.dest));
+}
+
+function initSitemap() {
+  return gulp
+    .src(paths.html.src, { read: false })
+    .pipe(
+      sitemap({
+        siteUrl: 'https://sitename.com',
+        changefreq: 'weekly',
+        priority: function (file) {
+          return file.relative === 'index.html' ? 1.0 : 0.9;
+        },
+      })
+    )
+    .pipe(gulp.dest(paths.html.dest));
 }
 
 function otherFiles() {
@@ -137,6 +195,7 @@ function watchFiles() {
   gulp.watch(paths.fonts.src, fonts);
   gulp.watch(paths.css.src, css);
   gulp.watch(paths.scss.dev.src, scss);
+  gulp.watch(paths.js.src, js);
   gulp.watch(paths.images.src, images);
 }
 
@@ -152,14 +211,21 @@ function isDev() {
   return process.env.NODE_ENV === 'dev';
 }
 
+function checkJsDirectory() {
+  const dirPath = path.join(__dirname, 'src', 'js');
+  return fs.existsSync(dirPath);
+}
+
 const build = gulp.series(
-  gulp.series(clean, html, fonts, scss, images, otherFiles)
+  gulp.series(clean, html, initSitemap, fonts, scss, js, images, otherFiles)
 );
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
+exports.initSitemap = initSitemap;
 exports.css = css;
 exports.scss = scss;
+exports.js = js;
 exports.images = images;
 exports.fonts = fonts;
 exports.otherFiles = otherFiles;
